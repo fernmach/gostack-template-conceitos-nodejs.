9989f0151199a269a9b631a90299b4ed9a29c8ed
Fernando Machado <netfm5@gmail.com>
2020-08-20 11:23:11 -0300

    Implementado armazenamento de repositórios(listagem, atualização e remoção) e possibilidade de receber likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,84 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  //TODO
+  //Validate URL
+  //Validate if "techs" is an array
+
+  const repo = {id: uuid(), title, url, techs, likes: 0};
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id }  = request.params;
+  const { title, url, techs } = request.body;
+  
+  //TODO  
+  //Test for valid repository identifier(uuid)
+  //Validate URL
+  //Validate if "techs" is an array
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id);
+
+  if( repoIndex < 0 ) {
+    return response.status(400).json( {error: "Repository not found"});
+  }
+
+  const repo = {
+    id: repositories[repoIndex].id, 
+    title, 
+    url, 
+    techs,
+    likes: repositories[repoIndex].likes
+  }
+
+  repositories[repoIndex] = repo;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  //TODO    
+  //Test for valid repository identifier(uuid)
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id);
+
+  if( repoIndex < 0 ) {
+    return response.status(400).json( {error: "Repository not found"});
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  //TODO    
+  //Test for valid repository identifier(uuid)
+
+  const repoIndex = repositories.findIndex( repo => repo.id === id);
+
+  if( repoIndex < 0 ) {
+    return response.status(400).json( {error: "Repository not found"});
+  }
+
+  repositories[repoIndex].likes += 1;
+
+  return response.json(repositories[repoIndex]);
+
 });
 
 module.exports = app;
